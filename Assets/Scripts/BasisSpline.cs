c4ad78162d4b46036c3c5de5401ea214671a604b
Brian Russin <btrussin@gmail.com>
2017-06-14 13:27:25 -0400

    working b-splines

--- Assets/Scripts/BasisSpline.cs
+++ Assets/Scripts/BasisSpline.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class BasisSpline : MonoBehaviour {
 
-    Vector3[] basisPoints;
+    public Vector3[] basisPoints;
     float[] basisWeights;
-    public int n = 0; // number of control points (zero-based)
-    public int m = 0; // number of weights
-    public int p = 0;
-    public int k = 0;
+    int n = 0; // number of control points (zero-based)
+    int m = 0; // number of weights
+    int p = 0;
 
     Vector3[] bsPoints;
 
@@ -18,38 +17,35 @@ public class BasisSpline : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
-        n = 3;
-        k = 4;
+        n = basisPoints.Length - 1;
 
-        basisPoints = new Vector3[n+1];
-
-        basisPoints[0] = new Vector3(0f, 0f, 0f);
-        basisPoints[1] = new Vector3(0f, 1f, 0f);
-        basisPoints[2] = new Vector3(1f, 1f, 0f);
-        basisPoints[3] = new Vector3(1f, 0f, 0f);
-
-        int numOutWts = k/2;
-        int numInnerWts = n;
-        m = numOutWts * 2 + numInnerWts;
+        int numOutWts = 4;
+        int numInnerWts = n-3;
+        m = numOutWts * 2 + numInnerWts - 1;
         basisWeights = new float[m+1];
         int i = 0;
         for( i = 0; i < numOutWts; i++ )
         {
             basisWeights[i] = 0f;
-            basisWeights[basisWeights.Length - 1 - i] = 1f;
+            basisWeights[m - i] = 1f;
         }
 
         float inc = 1f / (float)(numInnerWts+1);
-        for( int j = 1; j <= numInnerWts; j++ )
+        for( int j = 0; j < numInnerWts; j++ )
         {
-            basisWeights[i+j] = inc*j;
+            basisWeights[i+j] = inc*(j+1);
         }
 
-        p = Mathf.Max(m - n -1, 0);
+
+        p = m - n - 1;
+
+        Debug.Log("M: " + m);
+        Debug.Log("N: " + n);
+        Debug.Log("P: " + p);
 
 
         doStuff();
-        //populateLine();
+        populateLine();
     }
 
     void populateLine()
@@ -65,7 +61,7 @@ public class BasisSpline : MonoBehaviour {
 
     void doStuff()
     {
-        float[] uVals = new float[11];
+        float[] uVals = new float[100];
         float inc = 1f / (uVals.Length - 1);
         for( int i = 0; i < uVals.Length; i++ )
         {
@@ -88,12 +84,13 @@ public class BasisSpline : MonoBehaviour {
 
     Vector3 getPN(float u)
     {
+        if( u == 1f ) return basisPoints[n];
+
         Vector3 result = Vector3.zero;
         float val;
-        for(int i = 0; i < n; i++ )
+        for(int i = 0; i <= n; i++ )
         {
             val = getN(i, p, u);
-            Debug.Log("Val: " + val);
             result += basisPoints[i] * val;
         }
 
@@ -105,19 +102,31 @@ public class BasisSpline : MonoBehaviour {
         if( j == 0 )
         {
             if (basisWeights[i] == basisWeights[i + 1]) return 0f;
-            else if (i >= basisWeights[i] && i < basisWeights[i + 1]) return 1f;
+            else if (u >= basisWeights[i] && u < basisWeights[i + 1]) return 1f;
             else return 0f;
         }
-
+    
         float v1, v2;
 
-        if (basisWeights[i + j] == basisWeights[i]) v1 = 1f;
-        else v1 = (u - basisWeights[i]) / (basisWeights[i + j] - basisWeights[i]);
+        if( basisWeights[i + j] == basisWeights[i] )
+        {
+            v1 = 0f;
+        }
+        else
+        {
+            v1 = (u - basisWeights[i]) / (basisWeights[i + j] - basisWeights[i]) * getN(i, j - 1, u);
+        }
 
-        if (basisWeights[i + j + 1] == basisWeights[i + 1]) v2 = 1f;
-        else v2 = (basisWeights[i + j + 1] - u) / (basisWeights[i + j + 1] - basisWeights[i + 1]);
+        if (basisWeights[i + j + 1] == basisWeights[i + 1])
+        {
+            v2 = 0f;
+        }
+        else
+        {
+            v2 = (basisWeights[i + j + 1] - u) / (basisWeights[i + j + 1] - basisWeights[i + 1]) * getN(i + 1, j - 1, u);
+        }
 
-        return v1 * getN(i, j - 1, u) + v2 * getN(i + 1, j - 1, u);
+        return v1 + v2;
     }
 
    
