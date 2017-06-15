a43f583a73353007b6cd744d8bc4682c7764eb24
Brian Russin <btrussin@gmail.com>
2017-06-15 10:20:32 -0400

    implemented b-spline bar

--- Assets/Scripts/BasisSpline.cs
+++ Assets/Scripts/BasisSpline.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasisSpline : MonoBehaviour {
+public class BasisSpline : MonoBehaviour 
+{
+	public int numMajorDivisions = 100;
+    public int numMinorDivisions = 30;
+    public float radius = 0.02f;
+
+    public bool bar = false;
 
     public Vector3[] basisPoints;
     float[] basisWeights;
@@ -11,8 +17,10 @@ public class BasisSpline : MonoBehaviour {
     int p = 0;
 
     Vector3[] bsPoints;
+    Vector3[] bsTangents;
 
     public LineRenderer lineRend;
+    public MeshFilter meshFilter;
 
     // Use this for initialization
     void Start () {
@@ -44,8 +52,11 @@ public class BasisSpline : MonoBehaviour {
         Debug.Log("P: " + p);
 
 
-        doStuff();
-        populateLine();
+        calcBSplinePoints();
+        calcBSplineTangents();
+
+        if( bar ) populateMesh();
+		else populateLine();
     }
 
     void populateLine()
@@ -59,9 +70,9 @@ public class BasisSpline : MonoBehaviour {
 		
 	}
 
-    void doStuff()
+    void calcBSplinePoints()
     {
-        float[] uVals = new float[100];
+        float[] uVals = new float[numMajorDivisions];
         float inc = 1f / (uVals.Length - 1);
         for( int i = 0; i < uVals.Length; i++ )
         {
@@ -74,12 +85,38 @@ public class BasisSpline : MonoBehaviour {
         {
             bsPoints[i] = getPN(uVals[i]);
         }
+    }
 
-        for (int i = 0; i < uVals.Length; i++)
+    void calcBSplineTangents()
+    {
+        bsTangents = new Vector3[bsPoints.Length];
+
+        Vector3 prev, next;
+
+        for( int i = 0; i < bsPoints.Length; i++ )
         {
-            Debug.Log(i + ": " + bsPoints[i]);
-        }
+            if( i == 0 )
+            {
+                prev = bsPoints[1] - bsPoints[0];
+                next = prev;
+            }
+            else if( i == (bsPoints.Length-1) )
+            {
+                prev = bsPoints[bsPoints.Length-1] - bsPoints[bsPoints.Length-2];
+                next = prev;
+            }
+            else
+            {
+                prev = bsPoints[i] - bsPoints[i-1];
+                next = bsPoints[i+1] - bsPoints[i];
+            }
 
+            prev.Normalize();
+            next.Normalize();
+
+            bsTangents[i] = (prev + next) * 0.5f;
+            bsTangents[i].Normalize();
+        }
     }
 
     Vector3 getPN(float u)
@@ -127,6 +164,122 @@ public class BasisSpline : MonoBehaviour {
         }
 
         return v1 + v2;
+    }
+
+
+    public void populateMesh()
+    {
+
+        Vector3[] basePoints = bsPoints;
+        Vector3[] baseTangents = bsTangents;
+
+        Vector3 currVertex;
+        Vector3 tgtDirVector;
+        Vector3 currRight = Vector3.one;
+        Vector3 currForward;
+        Vector3 currUp;
+
+        Vector3 flatDirVector = basePoints[basePoints.Length-1] - basePoints[0];
+        flatDirVector.Normalize();
+
+        Vector3[] meshPoints = new Vector3[numMinorDivisions * numMajorDivisions];
+        Vector3[] meshNormals = new Vector3[numMinorDivisions * numMajorDivisions];
+
+        int currIdx = 0;
+        Vector3 prevUp = Vector3.zero;
+        Vector3 prevRight = Vector3.zero;
+
+        for (int i = 0; i < basePoints.Length; i++)
+        {
+            baseTangents[i].Normalize();
+
+            currForward = baseTangents[i];
+            
+            tgtDirVector = currForward;
+            for( int j = i+1; j < basePoints.Length; j++ )
+            {
+                tgtDirVector = basePoints[j] - basePoints[i];
+                tgtDirVector.Normalize();
+                float dot = Vector3.Dot(currForward, tgtDirVector);
+   
+                if (dot <= 0.95f )
+                {
+                    currRight = Vector3.Cross(tgtDirVector, currForward);
+                    break;
+                }
+            }
+
+            if (Mathf.Abs(Vector3.Dot(currForward, tgtDirVector)) > 0.95f)
+            {
+                currRight = Vector3.Cross(currForward, flatDirVector);
+            }
+            
+            if( i > 0 ) currRight = Vector3.Cross(currForward, prevUp);
+
+            currRight.Normalize();
+
+            if( i > 0 && Vector3.Dot(prevRight, currRight) < 0.0f )
+            {
+            	currRight = -currRight;
+            }
+          
+          	prevRight = currRight;
+
+            currUp = Vector3.Cross(currRight, currForward);
+            currUp.Normalize();
+
+            prevUp = currUp;
+
+            currVertex = currUp * radius;
+
+            Quaternion rotation = Quaternion.AngleAxis(360.0f / numMinorDivisions, currForward);
+
+            for (int j = 0; j < numMinorDivisions; j++)
+            {
+                meshPoints[currIdx] = currVertex + basePoints[i];
+                meshNormals[currIdx] = currVertex;
+                meshNormals[currIdx].Normalize();
+
+                currVertex = rotation * currVertex;
+                currIdx++;
+            }
+        }
+
+        int[] faceList = new int[(numMajorDivisions - 1)*(numMinorDivisions)*6];
+        int mainIdx = 0;
+        int k = 0;
+        for( int i = 0; i < numMajorDivisions-1; i++ )
+        {
+            for (int j = 0; j < (numMinorDivisions - 1); j++)
+            {
+                faceList[k++] = mainIdx + j;
+                faceList[k++] = mainIdx + j + 1;
+                faceList[k++] = mainIdx + j + numMinorDivisions + 1;
+
+                faceList[k++] = mainIdx + j;
+                faceList[k++] = mainIdx + j + numMinorDivisions + 1;
+                faceList[k++] = mainIdx + j + numMinorDivisions;
+            }
+
+            faceList[k++] = mainIdx + numMinorDivisions - 1;
+            faceList[k++] = mainIdx;
+            faceList[k++] = mainIdx + numMinorDivisions;
+
+            faceList[k++] = mainIdx + numMinorDivisions - 1;
+            faceList[k++] = mainIdx + numMinorDivisions;
+            faceList[k++] = mainIdx + numMinorDivisions + numMinorDivisions-1;
+
+            mainIdx += numMinorDivisions;
+        }
+
+       
+        Mesh mesh = new Mesh();
+        meshFilter.mesh = mesh;
+        mesh.vertices = meshPoints;
+        mesh.normals = meshNormals;
+        mesh.triangles = faceList;
+
+
     }
 
    
