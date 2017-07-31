2a1e8ee23b192272529c418c8e53b5605b7fdbdc
Brian Russin <btrussin@gmail.com>
2017-07-31 12:00:21 -0400

    added vive controller class and updating based on collisions

--- Assets/Scripts/CMJSONLoader.cs
+++ Assets/Scripts/CMJSONLoader.cs
@@ -86,7 +86,7 @@ public class CMJSONLoader : DataLoader {
 
         base.loadData();
 
-        procData();
+        //procData();
     }
 
 	private void procData()
---
+++ Assets/Scripts/NodeManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeManager : MonoBehaviour {
+
+    Color origColor = Color.white;
+    Material material = null;
+    MeshRenderer meshRend = null;
+
+    int numCollisions = 0;
+
+	// Use this for initialization
+	void Start () {
+        meshRend = gameObject.GetComponent<MeshRenderer>();
+        if( meshRend != null )
+        {
+            material = meshRend.material;
+
+            if (material != null)
+            {
+                origColor = material.color;
+            }
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        numCollisions++;
+        if (material == null) material = gameObject.GetComponent<Material>();
+        if (material != null) material.color = Color.white;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        numCollisions--;
+        if (numCollisions <= 0)
+        {
+            numCollisions = 0;
+            if (material != null) material.color = origColor;
+        }
+    }
+    
+
+}
